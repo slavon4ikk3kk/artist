07f456722b70080f18a763a03e20278cf23042ad
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-09-20 15:57:11 +0200

    add styles and new component

--- artisthub/src/App.js
+++ artisthub/src/App.js
@@ -5,7 +5,9 @@ import Artist from './components/artistList/Artist';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <Artist></Artist>
+      </div>
     </div>
   );
 }
--- artisthub/src/components/artistList/Artist.jsx
+++ artisthub/src/components/artistList/Artist.jsx
@@ -1,19 +1,37 @@
 import React, { useState, useEffect } from "react";
-
+import s from "./Artist.module.css";
 const Artist = () => {
-  const [artistList, setArtistList] = useState([]);
-  async function fetchArtists() {
-    const response = await fetch(
-      "https://sound-wave.b.goit.study/api/artists?limit=10&page=1"
-    );
-    const res = await response.json();
-    console.log(res);
-  }
-  useEffect(() => {
-    fetchArtists();
-  }, []);
+    const [artistList, setArtistList] = useState([]);
+    async function fetchArtists() {
+        const response = await fetch(
+            "https://sound-wave.b.goit.study/api/artists?limit=10&page=1"
+        );
+        const res = await response.json();
+        setArtistList(res.artists);
+    }
+    useEffect(() => {
+        fetchArtists();
+    }, []);
 
-  return <div></div>;
+    return (
+        <ul className={s.ul}>
+            {artistList.map((artist) => {
+                return <li>
+                    <img src={artist.strArtistThumb}></img>
+                    <ul className={s.ulGenre}>
+                      {artist.genres.map((genre)=>{
+                        return <li  className={s.genre}>{genre}</li>
+                      })}  
+                    </ul>
+                    <p className={s.name}>{artist.strArtist}</p>
+                    <p className={s.text}>{artist.strBiographyEN}</p>
+                    
+
+                </li>
+            })}
+
+        </ul>
+    );
 };
 
 export default Artist;
