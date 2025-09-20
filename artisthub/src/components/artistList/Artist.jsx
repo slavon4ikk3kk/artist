e19c01348b6263aa9db9b45e445f6f229747f839
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-09-20 16:20:45 +0300

    fix fetch

--- artisthub/src/components/artistList/Artist.jsx
+++ artisthub/src/components/artistList/Artist.jsx
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Artist = () => {
-    const [artistList, setArtistList] = useState([]);
-    useEffect(async() => {
-       const response = await fetch("https://sound-wave.b.goit.study/api/artists?limit=10&page=1");
-       const res = await response.json();
-       console.log(res)
-    }, [])
+  const [artistList, setArtistList] = useState([]);
+  async function fetchArtists() {
+    const response = await fetch(
+      "https://sound-wave.b.goit.study/api/artists?limit=10&page=1"
+    );
+    const res = await response.json();
+    console.log(res);
+  }
+  useEffect(() => {
+    fetchArtists();
+  }, []);
 
-    return (
-        <div></div>
-    )
-}
+  return <div></div>;
+};
 
-export default Artist
+export default Artist;
