3899848421cfcb464bc87770ee2d54fed3f4f274
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-10-04 14:04:49 +0300

    fix fetch artists

--- artisthub/src/components/artistList/Artist.jsx
+++ artisthub/src/components/artistList/Artist.jsx
@@ -1,42 +1,54 @@
 import React, { useState, useEffect } from "react";
 import s from "./Artist.module.css";
-import images from "../../assets/index.js"
+import images from "../../assets/index.js";
 const Artist = () => {
-    const [artistList, setArtistList] = useState([]);
-    const [page, setPage] = useState(1)
-    async function fetchArtists() {
-        const response = await fetch(
-            `https://sound-wave.b.goit.study/api/artists?limit=10&page=${page}`
-        );
-        const res = await response.json();
-        setArtistList((prev)=>{
-           setArtistList([...prev])
-        });
-    }
-    useEffect(() => {
-        fetchArtists();
-    }, [page]);
-
-    return (
-        <ul className={s.ul}>
-            {artistList.map((artist) => {
-                return <li>
-                    <img src={artist.strArtistThumb}></img>
-                    <ul className={s.ulGenre}>
-                        {artist.genres.map((genre) => {
-                            return <li className={s.genre}>{genre}</li>
-                        })}
-                    </ul>
-                    <p className={s.name}>{artist.strArtist}</p>
-                    <p className={s.text}>{artist.strBiographyEN}</p>
-                    <div className={s.details}>
-                        <button className={s.detailText}>Learn more <img src={images.icon} className={s.arrow}></img></button>
-                    </div>
-                </li>
-            })}
-          <button className={s.loadmore} onClick={()=>{setPage(page + 1)}}>Load more</button>
-        </ul>
+  const [artistList, setArtistList] = useState([]);
+  const [page, setPage] = useState(1);
+  async function fetchArtists() {
+    const response = await fetch(
+      `https://sound-wave.b.goit.study/api/artists?limit=10&page=${page}`
     );
+    const res = await response.json();
+    setArtistList((prev) => {
+      return [...prev, ...res.artists];
+    });
+  }
+  useEffect(() => {
+    fetchArtists();
+  }, [page]);
+
+  return (
+    <ul className={s.ul}>
+      {artistList.length > 0 &&
+        artistList.map((artist) => {
+          return (
+            <li>
+              <img src={artist.strArtistThumb}></img>
+              <ul className={s.ulGenre}>
+                {artist.genres.map((genre) => {
+                  return <li className={s.genre}>{genre}</li>;
+                })}
+              </ul>
+              <p className={s.name}>{artist.strArtist}</p>
+              <p className={s.text}>{artist.strBiographyEN}</p>
+              <div className={s.details}>
+                <button className={s.detailText}>
+                  Learn more <img src={images.icon} className={s.arrow}></img>
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      <button
+        className={s.loadMore}
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Load more
+      </button>
+    </ul>
+  );
 };
 
 export default Artist;
