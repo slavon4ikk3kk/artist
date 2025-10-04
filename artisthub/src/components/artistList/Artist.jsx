3e290408de6427ec089cfd068804aceb7b1fc3b9
MaksimRomanovvv <109852966+MaxRomanov5@users.noreply.github.com>
2025-10-04 14:51:27 +0300

    add fetch single artist

--- artisthub/src/components/artistList/Artist.jsx
+++ artisthub/src/components/artistList/Artist.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import s from "./Artist.module.css";
 import images from "../../assets/index.js";
+import LearnMore from "../learnMore/LearnMore";
 const Artist = () => {
   const [artistList, setArtistList] = useState([]);
   const [page, setPage] = useState(1);
-  const [id, setId] = useState(null)
+  const [id, setId] = useState(null);
+  const [isModal, setIsModal] = useState(false);
   async function fetchArtists() {
     const response = await fetch(
       `https://sound-wave.b.goit.study/api/artists?limit=10&page=${page}`
@@ -19,36 +21,45 @@ const Artist = () => {
   }, [page]);
 
   return (
-    <ul className={s.ul}>
-      {artistList.length > 0 &&
-        artistList.map((artist) => {
-          return (
-            <li>
-              <img src={artist.strArtistThumb}></img>
-              <ul className={s.ulGenre}>
-                {artist.genres.map((genre) => {
-                  return <li className={s.genre}>{genre}</li>;
-                })}
-              </ul>
-              <p className={s.name}>{artist.strArtist}</p>
-              <p className={s.text}>{artist.strBiographyEN}</p>
-              <div className={s.details}>
-                <button className={s.detailText} onClick={()=>{setId(artist._id)}}>
-                  Learn more <img src={images.icon} className={s.arrow}></img>
-                </button>
-              </div>
-            </li>
-          );
-        })}
-      <button
-        className={s.loadMore}
-        onClick={() => {
-          setPage(page + 1);
-        }}
-      >
-        Load more
-      </button>
-    </ul>
+    <section>
+      <ul className={s.ul}>
+        {artistList.length > 0 &&
+          artistList.map((artist) => {
+            return (
+              <li>
+                <img src={artist.strArtistThumb}></img>
+                <ul className={s.ulGenre}>
+                  {artist.genres.map((genre) => {
+                    return <li className={s.genre}>{genre}</li>;
+                  })}
+                </ul>
+                <p className={s.name}>{artist.strArtist}</p>
+                <p className={s.text}>{artist.strBiographyEN}</p>
+                <div className={s.details}>
+                  <button
+                    className={s.detailText}
+                    onClick={() => {
+                      setIsModal(true);
+                      setId(artist._id);
+                    }}
+                  >
+                    Learn more <img src={images.icon} className={s.arrow}></img>
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        <button
+          className={s.loadMore}
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          Load more
+        </button>
+      </ul>
+      {isModal && <LearnMore id={id}></LearnMore>}
+    </section>
   );
 };
 
--- artisthub/src/components/learnMore/LearnMore.jsx
+++ artisthub/src/components/learnMore/LearnMore.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import s from "./LearnMore.module.css";
 
-const LearnMore = () => {
+const LearnMore = ({ id }) => {
+  const [artist, setArtist] = useState(null);
+  const [albums, setalbums] = useState(null);
+  async function fetchArtist() {
+    const response = await fetch(
+      `https://sound-wave.b.goit.study/api/artists/${id}`
+    );
+    const res = await response.json();
+    setArtist(res);
+  }
+  useEffect(() => {
+    fetchArtist();
+  }, [id]);
+
+  async function fetchAlbums() {
+    const response = await fetch(
+      `https://sound-wave.b.goit.study/api/artists/${id}/albums`
+    );
+    const res = await response.json();
+    setalbums(res);
+  }
+  useEffect(() => {
+    fetchAlbums();
+  }, [id]);
   return (
-    <div>
-      
+    <div className={s.overlay}>
+      <div className={s.modal}></div>
     </div>
-  )
-}
-export default LearnMore
+  );
+};
+export default LearnMore;
