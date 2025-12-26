d78d6648dc9c0d13fa86a31874173d99313ff7be
slavik <151384840+slavon4ikk3kk@users.noreply.github.com>
2025-12-26 22:41:56 +0200

    add new components at modal

--- artisthub/src/components/feedbacks/FeedBacks.jsx
+++ artisthub/src/components/feedbacks/FeedBacks.jsx
@@ -10,6 +10,10 @@ import "swiper/css/scrollbar";
 
 const FeedBacks = () => {
   const [feedbacks, setFeedbacks] = useState([]);
+  const [isModal, setIsModal] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [isInput, setIsInput] = useState("");
+  const [isTextArea, setIsTextArea] = useState("");
   async function fetchFeedBacks() {
     const response = await fetch(
       "https://sound-wave.b.goit.study/api/feedbacks?limit=5&page=1"
@@ -32,6 +36,44 @@ const FeedBacks = () => {
       }
     }
     return arr;
+  }
+  function handleInputChange(e){
+   setIsInput(e.target.value);
+  }
+  function handleTextAreaChange(e){
+    setIsTextArea(e.target.value);
+  }
+  function handleStarsChange(value){
+     setRating(value);
+  }
+ async function sendFeedBack(){
+   try {
+    const response = await fetch(
+      "https://sound-wave.b.goit.study/api/feedbacks",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: isInput,
+          descr: isTextArea,
+          rating: rating,
+        }),
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+
+    setIsModal(false);
+    setIsInput("");
+    setIsTextArea("");
+    setRating(0);
+    fetchFeedBacks();
+  } catch (error) {
+    console.error("error", error);
+  }
   }
   return (
     <div className={s.swiperContainer}>
@@ -60,7 +102,25 @@ const FeedBacks = () => {
             );
           })}
         </Swiper>
+        <button className={s.buttonFeedback} onClick={() => { setIsModal(true) }}>Leave Feedback</button>
       </div>
+      {isModal && <div className={s.overlay}>
+        <div className={s.modal}>
+          <img src={images.icon} className={s.cross} onClick={()=>{setIsModal(false)}}></img>
+          <p className={s.submit}>Submit feedback</p>
+          <label className={s.label}>Name</label>
+          <input placeholder="Emily" className={s.inputName} type="text" onChange={handleInputChange} value={isInput}></input>
+          <label className={s.label}>Message</label>
+          <textarea placeholder="Type your message..." className={s.textarea} rows={4} onChange={handleTextAreaChange} value={isTextArea}></textarea>
+        </div>
+        <div className={s.starList}>
+           {[1,2,3,4,5].map((star)=>{
+            <img key={star} src={star <= rating ? images.purpleStar : images.star} className={s.star} onClick={handleStarsChange(star)}  />
+           })}
+        </div>
+        <button className={s.buttonSubmit} type="Submit" onClick={sendFeedBack}></button>
+      </div>
+      }
     </div>
   );
 };
